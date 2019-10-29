799611091f1a3857c8350967eb1c26eddd63697a
banatihajnalka <banati.hajnalka@gmail.com>
2019-10-29 09:28:44 +0100

    Updated .gitignore

---
+++ week-04/day-02/src/Doubling.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Doubling {
+    public static void main(String[] args) {
+        int[] baseNum = {123};
+        int[] baseNumDoubled = doubling(baseNum);
+        System.out.println(Arrays.toString(baseNumDoubled));
+    }
+
+    private static int[] doubling(int[] baseNum) {
+        
+    }
+
+}
