f558cfc82d9ba02dff281108be49f75d296defd6
banatihajnalka <banati.hajnalka@gmail.com>
2019-11-15 14:27:05 +0100

    Unique works without using function

--- Functions, Hard Ones/src/Unique.java
+++ Functions, Hard Ones/src/Unique.java
@@ -4,18 +4,16 @@ public class Unique {
     public static void main(String[] args) {
         //  Create a function that takes a list of numbers as a parameter
         //  Returns a list of numbers where every number in the list occurs only once
-
         //  Example
         //  System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
         //  should print: `[1, 11, 34, 52, 61]`
 
         int intArray[] = {1, 11, 34, 11, 52, 61, 1, 34};
-        
+
         HashMap<Integer, Integer> findUnique = new HashMap<Integer, Integer>(intArray.length);
         for (int i = 0; i < intArray.length; i++) {
             findUnique.put(intArray[i], i);
         }
         System.out.println(findUnique.keySet());
-
     }
 }
