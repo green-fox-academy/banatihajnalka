b4b104cf5823bb288e32c158d64d3b1dbef82fb8
banatihajnalka <banati.hajnalka@gmail.com>
2019-11-21 16:50:40 +0100

    Merge two arraylists into a new, using the same indexes, first from both goes to the first place

--- Data structure, Old ones, Optional/src/MatchMaking.java
+++ Data structure, Old ones, Optional/src/MatchMaking.java
@@ -10,16 +10,15 @@ public class MatchMaking {
             ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
             ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
 
-            HashMap<String, String> mappedList = new HashMap<>();
             System.out.println(makingMatches(girls, boys));
-  //         System.out.println(makingMatches(mappedList));
         }
 
-    private static HashMap<String, String> makingMatches(ArrayList<String> girls, ArrayList<String> boys) {
-        HashMap<String, String> mappedList = new HashMap<>();
+    private static ArrayList<String> makingMatches(ArrayList<String> girls, ArrayList<String> boys) {
+        ArrayList<String> mappedList = new ArrayList<>();
         for (int i = 0; i < girls.size(); i++) {
-            mappedList.put(girls.get(i), boys.get(i));
-        } return mappedList;
-
-}
+            mappedList.add(girls.get(i));
+            mappedList.add(boys.get(i));
+        }
+        return mappedList;
+    }
 }
