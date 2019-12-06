088a68a63c33369041853226cc4f06e301aac033
banatihajnalka <banati.hajnalka@gmail.com>
2019-12-06 11:33:04 +0100

    feat. (AppMusic) cleaned

--- Interfaces, Abstract classes/src/main/java/music/Instrument.java
+++ Interfaces, Abstract classes/src/main/java/music/Instrument.java
@@ -6,9 +6,7 @@ public abstract class Instrument {
     String getName() {
         return name;
     }
-
-    Instrument() {
-    }
+    
     Instrument(String name) {
         this.name = name;
     }
