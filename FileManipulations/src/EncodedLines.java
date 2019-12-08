c3ddc3542d3d9c9f6ae27a8de0087d6dd23347df
banatihajnalka <banati.hajnalka@gmail.com>
2019-12-08 19:39:46 +0100

    feat. (EncodedLines) 2D array added in shiftedCar method

--- FileManipulations/src/EncodedLines.java
+++ FileManipulations/src/EncodedLines.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class EncodedLines {
@@ -12,32 +11,32 @@ public class EncodedLines {
         String filename = "encodedLines.txt";
         List<String> text = readFile(filename);
 
-        List<Character> characterList = shiftedChar(text);
+        List<List<Character>> characterList = shiftedChar(text);
         System.out.println(characterList);
 
         String decryptedString = charListToString(characterList);
         System.out.println(decryptedString);
     }
 
-    private static String charListToString(List<Character> characterList) {
+    private static String charListToString(List<List<Character>> characterList) {
        StringBuilder builder = new StringBuilder();
-       for (Character ch : characterList) {
+       for (List<Character> ch : characterList) {
            builder.append(ch);
         }
-        String decryptedString = builder.toString().replace("\u001F", " ");
-       return decryptedString;
+        return builder.toString().replace("\u001F", " ");
     }
 
-    private static List<Character> shiftedChar(List<String> text) {
-        List<Character> characterList = new ArrayList<>();
+    private static List<List<Character>> shiftedChar(List<String> text) {
+        List<List<Character>> characterList = new ArrayList<>();
         for (int i = 0; i < text.size() ; i++) {
+            List<Character> currentCharacters = new ArrayList<>();
             for (int j = 0; j < text.get(i).length(); j++) {
-                char characters = ' ';
-                characters = text.get(i).charAt(j);
-                characters--;
+                char currentChar = text.get(i).charAt(j);
+                currentChar--;
+                currentCharacters.add(currentChar);
 //                itt hozza kell tenni, hogy vesszoket ne tegye at
-                characterList.add(characters);
             }
+            characterList.add(currentCharacters);
         } return characterList;
     }
 
