616940abc87b95bb2d50e5484676408002c7f38d
banatihajnalka <banati.hajnalka@gmail.com>
2019-12-10 11:12:58 +0100

    feat.(Reservation) ID generator tested

--- Interfaces, Abstract classes/src/reservation/MainReservation.java
+++ Interfaces, Abstract classes/src/reservation/MainReservation.java
@@ -4,7 +4,9 @@ public class MainReservation {
     public static void main(String[] args) {
 
         Reservation first = new Reservation();
+        Reservation second = new Reservation();
 
         System.out.println(first);
+        System.out.println(second);
     }
 }
