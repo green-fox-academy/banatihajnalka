613b8f0fb9a9a167678428c727600523b82bda33
banatihajnalka <banati.hajnalka@gmail.com>
2020-02-10 14:11:47 +0100

    feat.(SpringStart) Atomiclong counter added

--- SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
     private static final String template = "Hello, %s";
+    private static AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value="/greeting", method= RequestMethod.GET)
     public Greeting greeting(@RequestParam(value= "name", defaultValue = "World") String name) {
-        return new Greeting(1, String.format(template, name));
+        return new Greeting( counter.incrementAndGet(), String.format(template, name));
     }
 }
