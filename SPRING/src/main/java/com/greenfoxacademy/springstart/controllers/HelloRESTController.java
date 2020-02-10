ef2746d201e4f81b3e33a0498de9d66ce4a8b2fa
banatihajnalka <banati.hajnalka@gmail.com>
2020-02-10 14:08:43 +0100

    feat.(SpringStart) @RequestParam annotation

--- SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,13 +2,16 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
+    private static final String template = "Hello, %s";
+
     @RequestMapping(value="/greeting", method= RequestMethod.GET)
-    public Greeting greeting() {
-        return new Greeting(1, "Hello, World!");
+    public Greeting greeting(@RequestParam(value= "name", defaultValue = "World") String name) {
+        return new Greeting(1, String.format(template, name));
     }
 }
