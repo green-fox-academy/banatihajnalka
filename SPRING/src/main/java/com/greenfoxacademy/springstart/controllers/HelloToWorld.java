0490c279412fba68abf7593197de65f43e2f33c4
banatihajnalka <banati.hajnalka@gmail.com>
2020-02-11 12:21:37 +0100

    feat.(SpringStart) HelloToWorld added

---
+++ SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToWorld.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@Controller
+public class HelloToWorld {
+
+    Random random = new Random();
+
+    private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    @RequestMapping("/hello/world")
+    public String greeting(@RequestParam String color, @RequestParam Integer size, Model model) {
+        model.addAttribute("color", color);
+        model.addAttribute("size", size);
+        model.addAttribute("hello", hellos[new Random().nextInt(hellos.length)]);
+        return "greeting";
+    }
+}
--- SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/WebGreetCounter.java
+++ SPRING/src/main/java/com/greenfoxacademy/springstart/controllers/WebGreetCounter.java
@@ -1,7 +1,6 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
