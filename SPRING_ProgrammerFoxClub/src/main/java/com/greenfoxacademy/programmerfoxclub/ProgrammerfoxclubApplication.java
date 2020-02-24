4c88986e9c2fd42d7f11cdf210cff363450bc749
banatihajnalka <banati.hajnalka@gmail.com>
2020-02-24 10:31:05 +0100

    feat.(programmerFoxClub) index html added to 8080

---
+++ SPRING_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/ProgrammerfoxclubApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.programmerfoxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerfoxclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammerfoxclubApplication.class, args);
+    }
+
+}
---
+++ SPRING_ProgrammerFoxClub/src/test/java/com/greenfoxacademy/programmerfoxclub/ProgrammerfoxclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.programmerfoxclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerfoxclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
