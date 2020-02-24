dbdc13334d60b6f8aea13dca1171fda244c35263
banatihajnalka <banati.hajnalka@gmail.com>
2020-02-24 11:08:05 +0100

    feat.(programmerFoxClub) login html added

--- SPRING_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ SPRING_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -1,13 +1,33 @@
-//package com.greenfoxacademy.programmerfoxclub.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class MainController {
-//
-//    @GetMapping("/information")
-//    public String showMain() {
-//        return "information";
-//    }
-//}
+package com.greenfoxacademy.programmerfoxclub.controllers;
+
+        import com.greenfoxacademy.programmerfoxclub.modles.Fox;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    private Fox fox;
+
+    public MainController(){
+        this.fox = new Fox();
+    }
+
+    @GetMapping("/")
+    public String showMain() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String renderMain() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam (required = true) String name) {
+        return "redirect:/?name=" + name;
+    }
+}
---
+++ SPRING_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/modles/Fox.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.programmerfoxclub.modles;
+
+public class Fox {
+
+    private String name;
+
+    public Fox() {
+    }
+
+    public Fox(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
