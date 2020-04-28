2f4fa18bb1168f1fa3bf5f36121f5cb79760cbf4
banatihajnalka <banati.hajnalka@gmail.com>
2020-04-28 09:32:43 +0200

    SockMerchant ex. explanation added

--- HR/src/SockMerchant.java
+++ HR/src/SockMerchant.java
@@ -1,3 +1,10 @@
+//John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
+//        Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+//        For example, there are  socks with colors . There is one pair of color  and one of color .
+//        There are three odd socks left, one of each color. The number of pairs is .
+
+
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
