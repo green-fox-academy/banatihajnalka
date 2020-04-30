4b041eb648cd267d373d1e1d6d03fb3a131df6ae
banatihajnalka <banati.hajnalka@gmail.com>
2020-04-30 21:16:53 +0200

    init project

---
+++ SPRING ADVANCED/THIRD_API/src/main/java/com/greenfoxacademy/thirdapi/ThirdapiApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.thirdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdapiApplication.class, args);
+    }
+
+}
