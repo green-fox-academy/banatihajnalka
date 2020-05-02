8fc89f9f782f87cfbef1dd2c9946b6e9af551236
banatihajnalka <banati.hajnalka@gmail.com>
2020-05-02 12:22:21 +0200

    init project

---
+++ SPRING ADVANCED/WeekZero/src/main/java/com/greenfoxacademy/apicallingapi/ApicallingapiApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.apicallingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicallingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApicallingapiApplication.class, args);
+    }
+
+}
---
+++ SPRING ADVANCED/WeekZero/src/test/java/com/greenfoxacademy/apicallingapi/ApicallingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.apicallingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicallingapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
