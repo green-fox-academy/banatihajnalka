c0a4594d634ffbdcb0859cb84fe49128f8c0bbd5
banatihajnalka <banati.hajnalka@gmail.com>
2020-05-06 20:11:47 +0200

    init project

---
+++ SPRING ADVANCED/ApiCallingApi/src/main/java/com/greenfoxacademy/covidapi/CovidapiApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.covidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidapiApplication.class, args);
+    }
+
+}
